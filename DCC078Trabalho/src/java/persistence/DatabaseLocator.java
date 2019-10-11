13de8c88b7945d6e5052afb24b86fc817b774c47
Jônatas <jjsfandre@gmail.com>
2019-10-11 18:53:12 -0300

    Incluindo DatabaseLocator

---
+++ DCC078Trabalho/src/java/persistence/DatabaseLocator.java
@@ -0,0 +1,31 @@
+/*
+ * Padrão de projeto Singleton.
+ * Este padrão garante a existência de apenas uma instância de uma classe, 
+ * mantendo um ponto global de acesso ao seu objeto.
+ * exemplo: priavate static, public static getInstance
+ */
+package persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Anderson
+ */
+public class DatabaseLocator {
+    private static DatabaseLocator instance = new DatabaseLocator();
+    public static DatabaseLocator getInstance(){
+        return instance;
+    }
+    public Connection getConection() throws SQLException, ClassNotFoundException{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn =
+                DriverManager.getConnection("jdbc:mysql://remotemysql.com/KQ661Av6Jf", "KQ661Av6Jf", "Pwb3weObef");
+        Statement st = conn.createStatement();
+        return conn;
+    }
+    
+}
