2f641781f494df5f46404b5faf87cf2dc9b86956
JoaoPSJC <pedrocxm@gmail.com>
2019-10-22 12:32:35 -0300

    adicionados outros construtores para maior flexibilidade

--- DCC078Trabalho/src/java/model/Cliente.java
+++ DCC078Trabalho/src/java/model/Cliente.java
@@ -5,24 +5,48 @@
  */
 package model;
 
+import java.util.List;
+
 /**
  *
  * @author andradeld
  */
 public class Cliente extends Usuario {
-    private int CPF;
+    private final int CPF;
 
     public Cliente(int CPF, Long id, String nome, String login, String senha) {
         super(id, nome, login, senha);
         this.CPF = CPF;
     }
 
-    public int getCPF() {
-        return CPF;
+    public Cliente(int CPF, Long id, String nome, String login, List<DadoBancario> dadosBancarios, String senha) {
+        super(id, nome, login, dadosBancarios, senha);
+        this.CPF = CPF;
     }
 
-    public void setCPF(int CPF) {
+    public Cliente(int CPF, Long id, String nome, String login, String senha, List<Endereco> enderecos) {
+        super(id, nome, login, senha, enderecos);
         this.CPF = CPF;
+    }
+
+    public Cliente(int CPF, Long id, String nome, List<Contato> contatos, String login, String senha) {
+        super(id, nome, contatos, login, senha);
+        this.CPF = CPF;
+    }
+
+    public Cliente(int CPF, Long id, List<Pedido> pedidos, String nome, String login, String senha) {
+        super(id, pedidos, nome, login, senha);
+        this.CPF = CPF;
+    }
+
+    public Cliente(int CPF, Long id, String nome, String login, String senha, List<DadoBancario> dadosBancarios, List<Endereco> enderecos, List<Contato> contatos, List<Pedido> pedidos) {
+        super(id, nome, login, senha, dadosBancarios, enderecos, contatos, pedidos);
+        this.CPF = CPF;
+    }
+
+    
+    public int getCPF() {
+        return CPF;
     }
     
 }
--- DCC078Trabalho/src/java/model/Usuario.java
+++ DCC078Trabalho/src/java/model/Usuario.java
@@ -5,18 +5,143 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author andradeld
  */
 public abstract class Usuario {
-    private Long id;
-    private String nome;
-    private Endereco endereco;
-    private Contato contato;
-    
-    public Usuario(Long id, String nome){
-        this.id=id;
-        this.nome=nome;
+    private final Long id;
+    private String nome,
+            login,
+            senha;
+    private final List<DadoBancario> dadosBancarios;
+    private final List<Endereco> enderecos;
+    private final List<Contato> contatos;
+    private final List<Pedido> pedidos;
+
+    public Usuario(Long id, String nome, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        dadosBancarios = new ArrayList<>();
+        enderecos = new ArrayList<>();
+        contatos = new ArrayList<>();
+        pedidos = new ArrayList<>();
+    }
+    public Usuario(Long id, String nome, String login, List<DadoBancario> dadosBancarios, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.dadosBancarios = dadosBancarios;
+        enderecos = new ArrayList<>();
+        contatos = new ArrayList<>();
+        pedidos = new ArrayList<>();
+    }
+    public Usuario(Long id, String nome, String login, String senha, List<Endereco> enderecos) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        dadosBancarios = new ArrayList<>();
+        this.enderecos = enderecos;
+        contatos = new ArrayList<>();
+        pedidos = new ArrayList<>();
+    }
+    public Usuario(Long id, String nome,List<Contato> contatos, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        dadosBancarios = new ArrayList<>();
+        enderecos = new ArrayList<>();
+        this.contatos = contatos;
+        pedidos = new ArrayList<>();
+    }
+    public Usuario(Long id,List<Pedido> pedidos, String nome, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        dadosBancarios = new ArrayList<>();
+        enderecos = new ArrayList<>();
+        contatos = new ArrayList<>();
+        this.pedidos = pedidos;
+    }
+    public Usuario(Long id, String nome, String login, String senha,
+             List<DadoBancario> dadosBancarios, List<Endereco> enderecos,
+             List<Contato> contatos,List<Pedido> pedidos) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.dadosBancarios = dadosBancarios;
+        this.enderecos = enderecos;
+        this.contatos = contatos;
+        this.pedidos = pedidos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public List<DadoBancario> getDadosBancarios() {
+        return dadosBancarios;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public List<Contato> getContatos() {
+        return contatos;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public void addContato(Contato novoContato)
+    {
+        contatos.add(novoContato);
+    }
+    public void addDadoBancario(DadoBancario novoDadoBancario)
+    {
+        dadosBancarios.add(novoDadoBancario);
+    }
+    public void addEndereco(Endereco novoEndereco)
+    {
+        enderecos.add(novoEndereco);
+    }
+    public void addPedido(Pedido novoPedido)
+    {
+        pedidos.add(novoPedido);
     }
 }
