07325c52dcb07f04422eae4f55f1fbcb9e1b0a04
JoaoPSJC <pedrocxm@gmail.com>
2019-10-22 13:46:27 -0300

    dados, contrutores, getters e setters básicos adicionados

--- DCC078Trabalho/src/java/model/Pedido.java
+++ DCC078Trabalho/src/java/model/Pedido.java
@@ -5,14 +5,93 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
 /**
  *
  * @author andradeld
  */
 public class Pedido {
-    private Long id;
-    private Produto produto;
+    //campos só para o banco de dados?
+    //private Entregador entregador;
+    //private Cliente cliente;
+    //private Empresa empresa;
+    
+    private final Long id;
+    private final List<Produto> produtos;
     private String observacao;
-    private Entregador entregador;
     private PedidoEstado estado;
+    private Endereco endereco;
+    private float frete,
+            precoProdutos;
+
+    public Pedido(Long id, List<Produto> produtos, String observacao,
+            Endereco endereco, float frete) {
+        this.id = id;
+        this.produtos = produtos;
+        this.observacao = observacao;
+        this.endereco = endereco;
+        this.frete = frete;
+        precoProdutos = 0;
+        Iterator<Produto> produtoIterator = produtos.iterator();
+        while(produtoIterator.hasNext())
+        {
+            Produto produtoAtual = produtoIterator.next();
+            precoProdutos+= produtoAtual.getPreco();
+        }
+    }
+    public Pedido(Long id, String observacao,
+            Endereco endereco, float frete) {
+        this.id = id;
+        this.produtos = new ArrayList<>();
+        this.observacao = observacao;
+        this.endereco = endereco;
+        this.frete = frete;
+        precoProdutos = 0;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public PedidoEstado getEstado() {
+        return estado;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public float getFrete() {
+        return frete;
+    }
+
+    public float getPrecoProdutos() {
+        return precoProdutos;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public void setFrete(float frete) {
+        this.frete = frete;
+    }
+
+    public void addProduto(Produto novoProduto)
+    {
+        produtos.add(novoProduto);
+        precoProdutos+= novoProduto.getPreco();
+    }
+    
 }
