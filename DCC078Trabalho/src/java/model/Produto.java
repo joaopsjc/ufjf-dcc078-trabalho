e006a3b2177f21f5bddef35fd33f1a32913adfe1
JoaoPSJC <pedrocxm@gmail.com>
2019-10-22 15:07:27 -0300

    dados, contrutores, getters e setters básicos adicionados

--- DCC078Trabalho/src/java/model/Produto.java
+++ DCC078Trabalho/src/java/model/Produto.java
@@ -10,12 +10,60 @@ package model;
  * @author andradeld
  */
 public class Produto {
-    private Long id;
+    private final Long id;
     private String nome;
     private String descricao;
     private String categoria;
     private Double preco;
-    protected Promocao promocao;
-    
+    private produtoEstado estado;
+
+    public Produto(Long id, String nome, String descricao, String categoria, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.preco = preco;
+        this.estado = new produtoEstadoDisponivel();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public produtoEstado getEstado() {
+        return estado;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
     
 }
