c05dc5ae94cbdc6bd22c07a517d0bd58826d1451
JoaoPSJC <pedrocxm@gmail.com>
2019-10-28 11:13:00 -0300

    insert contato feito

--- DCC078Trabalho/src/java/persistence/ContatoDAO.java
+++ DCC078Trabalho/src/java/persistence/ContatoDAO.java
@@ -6,8 +6,10 @@
 package persistence;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import model.interfaces.Contato;
 
 /**
  *
@@ -17,5 +19,26 @@ public class ContatoDAO  extends DAO{
         private static ContatoDAO instance = new ContatoDAO();
     public static ContatoDAO getInstance(){
         return instance;
+    }
+    
+    public void insert(Contato contato, Long id_usuario) throws SQLException, ClassNotFoundException{
+        Connection conn = null;
+        PreparedStatement st = null;
+        try {
+            conn = DatabaseLocator.getInstance().getConection();
+            st = conn.prepareStatement("insert into usuario(id_usuario,valor,tipoContato) values (?,?,?)",Statement.RETURN_GENERATED_KEYS);
+            st.setLong(1,id_usuario);
+            st.setString(2,contato.getValor());
+            st.setString(3,contato.getTipo());
+            int affectedRows = st.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Creating user failed, no rows affected.");
+            }
+        } catch(SQLException e) {
+            throw e;
+        } finally {
+            closeResources(conn, st);
+        }
     }
 }
