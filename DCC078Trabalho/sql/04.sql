fb040fb3e00b29582b9de167d33aa2c70a0cdbf6
Jônatas André <jjsfandre@gmail.com>
2019-10-29 17:32:20 -0300

    Corrigindo sql

--- DCC078Trabalho/sql/04.sql
+++ DCC078Trabalho/sql/04.sql
@@ -7,6 +7,6 @@ CREATE TABLE promocaoPedidoProduto(
 	id_pedido INTEGER not null,
 	id_produto INTEGER not null,
 	id_promocao INTEGER not null,
-	CONSTRAINT fk_promocaoPedidoProduto_pedidoProduto FOREIGN KEY (id_pedido,id_produto) REFERENCES pedidoProduto
+	CONSTRAINT fk_promocaoPedidoProduto_pedidoProduto FOREIGN KEY (id_pedido,id_produto) REFERENCES pedidoProduto,
 	CONSTRAINT fk_promocaoPedidoProduto_promocao FOREIGN KEY (id_promocao) REFERENCES promocao
 );
