5da7c96fa9cf6261568fdd3ca8de3c0a2262200d
JoaoPSJC <jpcxm@hotmail.com>
2019-10-31 17:15:33 -0300

    lista pedidos disponíveis para o entregador

---
+++ DCC078Trabalho/src/java/action/ListaPedidosEntregadorDisponiveisAction.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package action;
+
+import controller.Action;
+import helper.Helper;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Pedido;
+import model.extensores.Entregador;
+
+/**
+ *
+ * @author jjsfa
+ */
+public class ListaPedidosEntregadorDisponiveisAction  implements Action{
+    @Override
+    public void execute(HttpServletRequest request,HttpServletResponse response)
+            throws IOException{ 
+        try{
+            
+            Entregador entregador = (Entregador) Helper.getInstance().getLoggedUser(request);
+            List<Pedido> listaPedidos = new ArrayList<>();
+            listaPedidos.add(entregador.getPedidoDisponivel());
+            request.setAttribute("listPedidos", listaPedidos);
+            request.getRequestDispatcher("Pedido/listaPedidos.jsp").forward(request, response);
+            
+        } catch(IOException | ServletException ex){
+            Logger.getLogger(ListaPedidosEntregadorDisponiveisAction.class.getName()).log(Level.SEVERE, null, ex);
+            response.sendRedirect("erro.jsp");
+        } 
+    }
+}
