ef8b016f1e80d70a523493afeeda81e3a88c5623
JoaoPSJC <pedrocxm@gmail.com>
2019-11-03 17:57:13 -0300

    mudança da forma como a categoria é apresentada

--- DCC078Trabalho/sql/2INSERTProduto.sql
+++ DCC078Trabalho/sql/2INSERTProduto.sql
@@ -1,15 +1,15 @@
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (3,'x-tudo','hamburguer','hamburguer',10,10,'Disponivel');/*20*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (6,'calabreza','pizza','pizza',10,10,'Disponivel');/*21*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (9,'salada','vegetariano','vegetariano',10,10,'Disponivel');/*22*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (12,'x-tudo','hamburguer','hamburguer',10,10,'Disponivel');/*23*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (15,'calabreza','pizza','pizza',10,10,'Disponivel');/*24*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (18,'salada','vegetariano','vegetariano',10,10,'Disponivel');/*25*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (18,'x-tudo','hamburguer','hamburguer',10,10,'Disponivel');/*26*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (18,'calabreza','pizza','pizza',10,10,'Disponivel');/*27*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (3,'salada','vegetariano','vegetariano',10,10,'Disponivel');/*28*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (6,'x-tudo','hamburguer','hamburguer',10,10,'Disponivel');/*29*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (9,'calabreza','pizza','pizza',10,10,'Disponivel');/*30*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (15,'salada','vegetariano','vegetariano',10,10,'Disponivel');/*31*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (18,'x-tudo','hamburguer','hamburguer',10,10,'Disponivel');/*32*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (6,'calabreza','pizza','pizza',10,10,'Disponivel');/*33*/
-insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (9,'salada','vegetariano','vegetariano',10,10,'Disponivel');/*34*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (3,'x-tudo','Sanduiche','Sanduiche',10,10,'Disponivel');/*20*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (6,'calabreza','Pizza','Pizza',10,10,'Disponivel');/*21*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (9,'Sanduiche e Pizza','Combo','Combo',10,10,'Disponivel');/*22*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (12,'x-tudo','Sanduiche','Sanduiche',10,10,'Disponivel');/*23*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (15,'calabreza','Pizza','Pizza',10,10,'Disponivel');/*24*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (18,'Sanduiche e Pizza','Combo','Combo',10,10,'Disponivel');/*25*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (18,'x-tudo','Sanduiche','Sanduiche',10,10,'Disponivel');/*26*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (18,'calabreza','Pizza','Pizza',10,10,'Disponivel');/*27*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (3,'Sanduiche e Pizza','Combo','Combo',10,10,'Disponivel');/*28*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (6,'x-tudo','Sanduiche','Sanduiche',10,10,'Disponivel');/*29*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (9,'calabreza','Pizza','Pizza',10,10,'Disponivel');/*30*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (15,'Sanduiche e Pizza','Combo','Combo',10,10,'Disponivel');/*31*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (18,'x-tudo','Sanduiche','Sanduiche',10,10,'Disponivel');/*32*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (6,'calabreza','Pizza','Pizza',10,10,'Disponivel');/*33*/
+insert into produto(id_empresa,nome,categoria,descricao,quantidade,preco,estado) values (9,'Sanduiche e Pizza','Combo','Combo',10,10,'Disponivel');/*34*/
