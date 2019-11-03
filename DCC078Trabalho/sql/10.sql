9b558ecb038eac07e1a1bb22ce74b73f6481c4e4
JoaoPSJC <pedrocxm@gmail.com>
2019-11-03 17:56:35 -0300

    tabela que liga combo a produto

---
+++ DCC078Trabalho/sql/10.sql
@@ -0,0 +1,8 @@
+CREATE TABLE ComboProduto (
+        id_combo INTEGER not null,
+        id_produto INTEGER not null,
+	CONSTRAINT fk_ComboProduto_combo FOREIGN KEY (id_combo) REFERENCES produto,
+	CONSTRAINT fk_ComboProduto_produto FOREIGN KEY (id_produto) REFERENCES produto
+);
+
+
