57fa7c15ed10c4dde1856a4dad5143ba31b430cb
JoaoPSJC <jpcxm@hotmail.com>
2019-11-05 19:21:41 -0300

    inserção de comboProduto feito

--- DCC078Trabalho/sql/4INSERTcomboProduto.sql
+++ DCC078Trabalho/sql/4INSERTcomboProduto.sql
@@ -1,5 +1,6 @@
-insert into comboProduto(id_produto,id_combo) values (20,28);
-insert into comboProduto(id_produto,id_combo) values (21,34);
-insert into comboProduto(id_produto,id_combo) values (29,34);
-insert into comboProduto(id_produto,id_combo) values (33,34);
-insert into comboProduto(id_produto,id_combo) values (30,22);
+insert into comboProduto(id_produto,id_combo) values (2,10);
+insert into comboProduto(id_produto,id_combo) values (12,4);
+insert into comboProduto(id_produto,id_combo) values (8,7);
+insert into comboProduto(id_produto,id_combo) values (9,7);
+insert into comboProduto(id_produto,id_combo) values (14,7);
+insert into comboProduto(id_produto,id_combo) values (6,13);
