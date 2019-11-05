5655e0961505e991ff08f3e8f828ddf96861e7f6
JoaoPSJC <jpcxm@hotmail.com>
2019-11-05 18:57:29 -0300

    inserção de combos no banco de dados

---
+++ DCC078Trabalho/sql/4INSERTcomboProduto.sql
@@ -0,0 +1,5 @@
+insert into comboProduto(id_produto,id_combo) values (20,28);
+insert into comboProduto(id_produto,id_combo) values (21,34);
+insert into comboProduto(id_produto,id_combo) values (29,34);
+insert into comboProduto(id_produto,id_combo) values (33,34);
+insert into comboProduto(id_produto,id_combo) values (30,22);
