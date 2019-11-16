b83925fc533e5f6798a367077bc0d4a3f6e8d3be
JoaoPSJC <pedrocxm@gmail.com>
2019-11-16 17:41:48 -0300

    biblioteca de classes incompleta, remover campo

--- DCC078Trabalho/src/java/model/implementadores/PromocaoVazia.java
+++ DCC078Trabalho/src/java/model/implementadores/PromocaoVazia.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.implementadores;
 
 import model.interfaces.Promocao;
 
-/**
- *
- * @author ice
- */
 public class PromocaoVazia implements Promocao{
     private Long id;
     private final String nome = "Sem Promoção";
