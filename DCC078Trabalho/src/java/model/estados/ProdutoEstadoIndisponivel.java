d7592948a139879b66c46862fa05ee87a7c57b9b
JoaoPSJC <pedrocxm@gmail.com>
2019-11-16 17:22:07 -0300

    excesso de comentários, remover comentário

--- DCC078Trabalho/src/java/model/estados/ProdutoEstadoIndisponivel.java
+++ DCC078Trabalho/src/java/model/estados/ProdutoEstadoIndisponivel.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.estados;
 
 import model.abstratos.Produto;
 import model.interfaces.ProdutoEstado;
 
-/**
- *
- * @author JoaoPSJC
- */
 public class ProdutoEstadoIndisponivel implements ProdutoEstado{
 
     @Override
