a187930d9038d0d581f8944c94f8994c455c7952
JoaoPSJC <pedrocxm@gmail.com>
2019-11-16 15:36:51 -0300

    remover comentários

--- DCC078Trabalho/src/java/model/abstratos/Usuario.java
+++ DCC078Trabalho/src/java/model/abstratos/Usuario.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.abstratos;
 
 import model.interfaces.Contato;
@@ -11,10 +6,6 @@ import java.util.Iterator;
 import java.util.List;
 import model.Pedido;
 
-/**
- *
- * @author andradeld
- */
 public abstract class Usuario {
     private Long id;
     private String nome,
