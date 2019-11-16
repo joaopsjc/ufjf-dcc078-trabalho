3871816d1d9b276d7b8e7b249cfb44990ff6f9f3
JoaoPSJC <pedrocxm@gmail.com>
2019-11-16 17:31:23 -0300

    excesso de comentários, remover comentário

--- DCC078Trabalho/src/java/model/estados/PedidoCancelado.java
+++ DCC078Trabalho/src/java/model/estados/PedidoCancelado.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.estados;
 
 import model.Pedido;
 import model.interfaces.PedidoEstado;
 
-/**
- *
- * @author ice
- */
 public class PedidoCancelado implements PedidoEstado{
     
     @Override
