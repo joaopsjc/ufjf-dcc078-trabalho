020ce040124b0e28a1abe6c5f18fa0efae2b8ffb
JoaoPSJC <pedrocxm@gmail.com>
2019-11-16 15:40:18 -0300

    remover comentários

--- DCC078Trabalho/src/java/model/extensores/Entregador.java
+++ DCC078Trabalho/src/java/model/extensores/Entregador.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.extensores;
 
 import helper.HelperPedido;
@@ -16,10 +11,6 @@ import model.EntregadorChainResponsibility;
 import model.Pedido;
 import persistence.PedidoDAO;
 
-/**
- *
- * @author andradeld
- */
 public class Entregador extends Usuario {
     private Entregador proxEntregador;
     private int avaliacao;
