10792e472984c2eb362568832733dacfa18bf1b7
JoaoPSJC <pedrocxm@gmail.com>
2019-11-16 18:17:05 -0300

    excesso de comentarios, remover comentario

--- DCC078Trabalho/src/java/model/ProdutoMemento.java
+++ DCC078Trabalho/src/java/model/ProdutoMemento.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 import model.estados.ProdutoEstadoIndisponivel;
 import model.interfaces.ProdutoEstado;
 
-/**
- *
- * @author ice
- */
 public class ProdutoMemento {
     private ProdutoEstado estado;
 
