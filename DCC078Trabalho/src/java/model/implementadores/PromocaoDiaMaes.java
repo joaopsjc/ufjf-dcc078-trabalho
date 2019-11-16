7918cd4f3710b963f6b18f473c224fb88576b750
JoaoPSJC <pedrocxm@gmail.com>
2019-11-16 17:42:19 -0300

    biblioteca de classes incompleta, remover campo

--- DCC078Trabalho/src/java/model/implementadores/PromocaoDiaMaes.java
+++ DCC078Trabalho/src/java/model/implementadores/PromocaoDiaMaes.java
@@ -1,18 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.implementadores;
 
 import model.interfaces.Promocao;
 
-/**
- *
- * @author ice
- */
 public class PromocaoDiaMaes implements Promocao{
-    private Long id;
     private final String nome = "Promoção Dia das Mães";
     private final String tipo = "DiaMaes";
     
