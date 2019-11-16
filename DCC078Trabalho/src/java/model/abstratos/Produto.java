97e94148c30e547b89da1d73d342376c0ae379fc
JoaoPSJC <pedrocxm@gmail.com>
2019-11-16 16:41:41 -0300

    excesso de comentários, remover comentário

--- DCC078Trabalho/src/java/model/abstratos/Produto.java
+++ DCC078Trabalho/src/java/model/abstratos/Produto.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.abstratos;
 
 import model.ProdutoMemento;
@@ -11,10 +6,6 @@ import model.estados.ProdutoEstadoDisponivel;
 import model.estados.ProdutoEstadoIndisponivel;
 import model.interfaces.ProdutoEstado;
 
-/**
- *
- * @author andradeld
- */
 public abstract class Produto {
     private Long id;
     private String nome,
