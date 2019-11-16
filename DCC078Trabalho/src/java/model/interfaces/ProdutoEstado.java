8a3f2762be9565f0b3677bcafaf0878817352749
JoaoPSJC <pedrocxm@gmail.com>
2019-11-16 17:22:16 -0300

    excesso de comentários, remover comentário

--- DCC078Trabalho/src/java/model/interfaces/ProdutoEstado.java
+++ DCC078Trabalho/src/java/model/interfaces/ProdutoEstado.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.interfaces;
 
 import model.abstratos.Produto;
 
-/**
- *
- * @author JoaoPSJC
- */
 public interface ProdutoEstado{
     public String getEstado();
     public boolean disponivel(Produto produto);
