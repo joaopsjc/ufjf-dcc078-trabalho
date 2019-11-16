069cb98021353c3a38d861f91ea21ce404891686
JoaoPSJC <pedrocxm@gmail.com>
2019-11-16 15:39:51 -0300

    remover comentários

--- DCC078Trabalho/src/java/model/extensores/Cliente.java
+++ DCC078Trabalho/src/java/model/extensores/Cliente.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.extensores;
 
 import java.sql.SQLException;
@@ -24,10 +19,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import persistence.NotificacaoDAO;
 
-/**
- *
- * @author andradeld
- */
 public class Cliente extends Usuario implements Observer {
     
     private Observable pedido;
