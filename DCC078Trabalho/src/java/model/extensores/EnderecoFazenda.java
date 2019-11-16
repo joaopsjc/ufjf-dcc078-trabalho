ae92f7ebad11ec4aca581116409a2f2bb573e48b
JoaoPSJC <pedrocxm@gmail.com>
2019-11-16 16:54:37 -0300

    biblioteca de classes incompleta, remover método

--- DCC078Trabalho/src/java/model/extensores/EnderecoFazenda.java
+++ DCC078Trabalho/src/java/model/extensores/EnderecoFazenda.java
@@ -13,9 +13,6 @@ import model.abstratos.Endereco;
  */
 public class EnderecoFazenda extends Endereco{
 
-    public EnderecoFazenda(String numero, String cep, long id, String lagradouro, String complemento, String bairro, String estado, String cidade) {
-        super(numero, cep, id, lagradouro, complemento, bairro, estado, cidade);
-    }
     public EnderecoFazenda(){
         super();
     }
