3fa3d2ed8ce4694f5e8264b0dfebbf7ad94e3c7f
Jônatas <jjsfandre@gmail.com>
2019-11-18 21:33:00 -0300

    Retirando códigos duplicados EnderecoDAO v2

--- DCC078Trabalho/src/java/persistence/EnderecoDAO.java
+++ DCC078Trabalho/src/java/persistence/EnderecoDAO.java
@@ -27,163 +27,54 @@ public class EnderecoDAO  extends DAO{
     }
     
     public void insert(Endereco endereco) throws SQLException, ClassNotFoundException{
-        Connection conn = null;
+        Connection conn = DatabaseLocator.getInstance().getConection();
         PreparedStatement st = null;
-        try {
-            conn = DatabaseLocator.getInstance().getConection();
-            st = conn.prepareStatement("insert into endereco(id_usuario,numero,complemento,logradouro,bairro,cidade,estado,tipoEndereco,cep) values (?,?,?,?,?,?,?,?,?)",Statement.RETURN_GENERATED_KEYS);
-            st.setLong(1,endereco.getId_usuario());
-            st.setString(2,endereco.getNumero());
-            st.setString(3,endereco.getComplemento());
-            st.setString(4,endereco.getLogradouro());
-            st.setString(5,endereco.getBairro());
-            st.setString(6,endereco.getCidade());
-            st.setString(7,endereco.getEstado());
-            st.setString(8,endereco.getTipo());
-            st.setString(9,endereco.getCep());
-            int affectedRows = st.executeUpdate();
-
-            if (affectedRows == 0) {
-                throw new SQLException("Creating endereco failed, no rows affected.");
-            }
-        } catch(SQLException e) {
-            throw e;
-        } finally {
-            closeResources(conn, st);
-        }
+        st = conn.prepareStatement("insert into endereco(id_usuario,numero,complemento,logradouro,bairro,cidade,estado,tipoEndereco,cep) values (?,?,?,?,?,?,?,?,?)",Statement.RETURN_GENERATED_KEYS);
+        st.setLong(1,endereco.getId_usuario());
+        st.setString(2,endereco.getNumero());
+        st.setString(3,endereco.getComplemento());
+        st.setString(4,endereco.getLogradouro());
+        st.setString(5,endereco.getBairro());
+        st.setString(6,endereco.getCidade());
+        st.setString(7,endereco.getEstado());
+        st.setString(8,endereco.getTipo());
+        st.setString(9,endereco.getCep());
+        executeQuery(st);
     }
     public Endereco getById(String id) throws SQLException, ClassNotFoundException{
         return getById(Long.parseLong(id));
     }
     
     public Endereco getById(Long id_endereco) throws SQLException, ClassNotFoundException{
-        Connection conn = null;
-        Statement st = null;
-        Endereco endereco = null;
-        try {
-            conn = DatabaseLocator.getInstance().getConection();
-            st = conn.createStatement();
-
-            ResultSet rs = st.executeQuery("select * from endereco where id="+id_endereco);
-
-            if (rs.next())
-            {
-                String numero = rs.getString("numero");
-                String complemento = rs.getString("complemento");
-                String logradouro = rs.getString("logradouro");
-                String bairro = rs.getString("bairro");
-                String cidade = rs.getString("cidade");
-                String estado = rs.getString("estado");
-                String tipoEndereco = rs.getString("tipoEndereco");
-                String cep = rs.getString("cep");
-                boolean isPrincipal = rs.getInt("isPrincipal")==1;
-                Long id_usuario = rs.getLong("id_usuario");
-                endereco = EnderecoFactory.create(tipoEndereco);
-                endereco.setId(id_endereco);
-                endereco.setNumero(numero);
-                endereco.setComplemento(complemento);
-                endereco.setLogradouro(logradouro);
-                endereco.setBairro(bairro);
-                endereco.setCidade(cidade);
-                endereco.setEstado(estado);
-                endereco.setCep(cep);
-                endereco.setPrincipal(isPrincipal);
-                endereco.setId_usuario(id_usuario);
-            }
-        } catch(SQLException e) {
-            throw e;
-        } finally {
-            closeResources(conn, st);
-        }
-        return endereco;
+        String query ="select * from endereco where id="+id_endereco;
+        return getSingleEndereco(query);
     }
 
     public List<Endereco> getEnderecosByUserId(Long id_usuario) throws SQLException, ClassNotFoundException{
-        Connection conn = null;
-        Statement st = null;
-        List<Endereco> enderecos = new ArrayList<>();
-        try {
-            conn = DatabaseLocator.getInstance().getConection();
-            st = conn.createStatement();
-
-            ResultSet rs = st.executeQuery("select * from endereco where id_usuario="+id_usuario);
-
-            while (rs.next())
-            {
-                Long id = rs.getLong("id");
-                String numero = rs.getString("numero");
-                String complemento = rs.getString("complemento");
-                String logradouro = rs.getString("logradouro");
-                String bairro = rs.getString("bairro");
-                String cidade = rs.getString("cidade");
-                String estado = rs.getString("estado");
-                String tipoEndereco = rs.getString("tipoEndereco");
-                String cep = rs.getString("cep");
-                boolean isPrincipal = rs.getInt("isPrincipal")==1;
-                Endereco novoEndereco = EnderecoFactory.create(tipoEndereco);
-                novoEndereco.setId(id);
-                novoEndereco.setNumero(numero);
-                novoEndereco.setComplemento(complemento);
-                novoEndereco.setLogradouro(logradouro);
-                novoEndereco.setBairro(bairro);
-                novoEndereco.setCidade(cidade);
-                novoEndereco.setEstado(estado);
-                novoEndereco.setCep(cep);
-                novoEndereco.setPrincipal(isPrincipal);
-                novoEndereco.setId_usuario(id_usuario);
-                enderecos.add(novoEndereco);
-            }
-        } catch(SQLException e) {
-            throw e;
-        } finally {
-            closeResources(conn, st);
-        }
-        return enderecos;
+        String query = "select * from endereco where id_usuario="+id_usuario;
+        return getListEnderecos(query);
     }
     
     public void update(Endereco endereco) throws SQLException, ClassNotFoundException{
-        Connection conn = null;
-        PreparedStatement st = null;
-        try {
-            conn = DatabaseLocator.getInstance().getConection();
-            st = conn.prepareStatement("update endereco set id_usuario=?,numero=?,complemento=?,logradouro=?,bairro=?,cidade=?,estado=?,tipoEndereco=?,cep=? where id=?",Statement.RETURN_GENERATED_KEYS);
-            st.setLong(1,endereco.getId_usuario());
-            st.setString(2,endereco.getNumero());
-            st.setString(3,endereco.getComplemento());
-            st.setString(4,endereco.getLogradouro());
-            st.setString(5,endereco.getBairro());
-            st.setString(6,endereco.getCidade());
-            st.setString(7,endereco.getEstado());
-            st.setString(8,endereco.getTipo());
-            st.setString(9,endereco.getCep());
-            st.setLong(10,endereco.getId());
-            int affectedRows = st.executeUpdate();
-
-            if (affectedRows == 0) {
-                throw new SQLException("Update endereco failed, no rows affected.");
-            }
-        } catch(SQLException e) {
-            throw e;
-        } finally {
-            closeResources(conn, st);
-        }
+        Connection conn = DatabaseLocator.getInstance().getConection();
+        PreparedStatement st = conn.prepareStatement("update endereco set id_usuario=?,numero=?,complemento=?,logradouro=?,bairro=?,cidade=?,estado=?,tipoEndereco=?,cep=? where id=?",Statement.RETURN_GENERATED_KEYS);
+        st.setLong(1,endereco.getId_usuario());
+        st.setString(2,endereco.getNumero());
+        st.setString(3,endereco.getComplemento());
+        st.setString(4,endereco.getLogradouro());
+        st.setString(5,endereco.getBairro());
+        st.setString(6,endereco.getCidade());
+        st.setString(7,endereco.getEstado());
+        st.setString(8,endereco.getTipo());
+        st.setString(9,endereco.getCep());
+        st.setLong(10,endereco.getId());
+        executeQuery(st);
+        
     }
     
     public void delete(Long id_endereco) throws SQLException, ClassNotFoundException{
-        Connection conn = null;
-        PreparedStatement st = null;
-        try {
-            conn = DatabaseLocator.getInstance().getConection();
-            st = conn.prepareStatement("delete from endereco where id="+id_endereco,Statement.RETURN_GENERATED_KEYS);
-            int affectedRows = st.executeUpdate();
-            if (affectedRows == 0) {
-                throw new SQLException("Delete endereco failed, no rows affected.");
-            }
-        } catch(SQLException e) {
-            throw e;
-        } finally {
-            closeResources(conn, st);
-        }
+        String query = "delete from endereco where id="+id_endereco;
+        executeQueryDelete(query);
     }
     
     private void delete(String id) throws SQLException, ClassNotFoundException{
@@ -196,33 +87,43 @@ public class EnderecoDAO  extends DAO{
     }
 
     public void setEnderecoPrincipal(Long id, Long idUsuario) throws SQLException, ClassNotFoundException {
+        Connection conn = DatabaseLocator.getInstance().getConection();
+        PreparedStatement st = conn.prepareStatement("update endereco set isPrincipal=0 where id_usuario=?",Statement.RETURN_GENERATED_KEYS);
+        st.setLong(1,idUsuario);
+        executeQuery(st,false);
+        st = conn.prepareStatement("update endereco set isPrincipal=1 where id=?",Statement.RETURN_GENERATED_KEYS);
+        st.setLong(1,id);            
+        executeQuery(st);
+    }
+
+    public Endereco getPrincipalByUserId(Long id_usuario) throws SQLException, ClassNotFoundException{
+        String query = "select * from endereco where isPrincipal=1 and id_usuario="+id_usuario;
+        return getSingleEndereco(query);
+    }
+    
+    public List<Endereco> getListEnderecos(String query) throws ClassNotFoundException, SQLException{
         Connection conn = null;
-        PreparedStatement st = null;
+        Statement st = null;
+        List<Endereco> enderecos = new ArrayList<>();
         try {
             conn = DatabaseLocator.getInstance().getConection();
-            st = conn.prepareStatement("update endereco set isPrincipal=0 where id_usuario=?",Statement.RETURN_GENERATED_KEYS);
-            st.setLong(1,idUsuario);
-            int affectedRows = st.executeUpdate();
-
-            if (affectedRows == 0) {
-                throw new SQLException("Update endereco failed, no rows affected.");
-            }
-            st = conn.prepareStatement("update endereco set isPrincipal=1 where id=?",Statement.RETURN_GENERATED_KEYS);
-            st.setLong(1,id);
+            st = conn.createStatement();
             
-            affectedRows = st.executeUpdate();
-
-            if (affectedRows == 0) {
-                throw new SQLException("Update endereco failed, no rows affected.");
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next())
+            {
+                Endereco novoEndereco = populateEnderecoObjectFromDataset(rs);
+                enderecos.add(novoEndereco);
             }
         } catch(SQLException e) {
             throw e;
         } finally {
             closeResources(conn, st);
         }
+        return enderecos;
     }
-
-    public Endereco getPrincipalByUserId(Long id_usuario) throws SQLException, ClassNotFoundException{
+    
+    public Endereco getSingleEndereco(String query) throws SQLException, ClassNotFoundException{
         Connection conn = null;
         Statement st = null;
         Endereco endereco = null;
@@ -230,31 +131,11 @@ public class EnderecoDAO  extends DAO{
             conn = DatabaseLocator.getInstance().getConection();
             st = conn.createStatement();
 
-            ResultSet rs = st.executeQuery("select * from endereco where isPrincipal=1 and id_usuario="+id_usuario);
+            ResultSet rs = st.executeQuery(query);
 
             if (rs.next())
             {
-                String numero = rs.getString("numero");
-                String complemento = rs.getString("complemento");
-                String logradouro = rs.getString("logradouro");
-                String bairro = rs.getString("bairro");
-                String cidade = rs.getString("cidade");
-                String estado = rs.getString("estado");
-                String tipoEndereco = rs.getString("tipoEndereco");
-                String cep = rs.getString("cep");
-                boolean isPrincipal = rs.getInt("isPrincipal")==1;
-                Long id_endereco = rs.getLong("id");
-                endereco = EnderecoFactory.create(tipoEndereco);
-                endereco.setId(id_endereco);
-                endereco.setNumero(numero);
-                endereco.setComplemento(complemento);
-                endereco.setLogradouro(logradouro);
-                endereco.setBairro(bairro);
-                endereco.setCidade(cidade);
-                endereco.setEstado(estado);
-                endereco.setCep(cep);
-                endereco.setPrincipal(isPrincipal);
-                endereco.setId_usuario(id_usuario);
+                endereco = populateEnderecoObjectFromDataset(rs);
             }
         } catch(SQLException e) {
             throw e;
@@ -263,5 +144,32 @@ public class EnderecoDAO  extends DAO{
         }
         return endereco;
     }
-
+    
+    public Endereco populateEnderecoObjectFromDataset(ResultSet rs) throws SQLException{
+        Endereco endereco = null;
+        String numero = rs.getString("numero");
+        String complemento = rs.getString("complemento");
+        String logradouro = rs.getString("logradouro");
+        String bairro = rs.getString("bairro");
+        String cidade = rs.getString("cidade");
+        String estado = rs.getString("estado");
+        String tipoEndereco = rs.getString("tipoEndereco");
+        String cep = rs.getString("cep");
+        boolean isPrincipal = rs.getInt("isPrincipal")==1;
+        Long id_usuario = rs.getLong("id_usuario");
+        Long id_endereco = rs.getLong("id");
+        
+        endereco = EnderecoFactory.create(tipoEndereco);
+        endereco.setId(id_endereco);
+        endereco.setNumero(numero);
+        endereco.setComplemento(complemento);
+        endereco.setLogradouro(logradouro);
+        endereco.setBairro(bairro);
+        endereco.setCidade(cidade);
+        endereco.setEstado(estado);
+        endereco.setCep(cep);
+        endereco.setPrincipal(isPrincipal);
+        endereco.setId_usuario(id_usuario);
+        return endereco;
+    }
 }
